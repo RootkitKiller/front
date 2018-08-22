636d138c477921c7f7eed84fcb8d0c8136bc729c
mooninwater <wingsnorth@hotmail.com>
2018-08-22 14:42:42 +0800

    添加ipfs接口测试 上传、获取

---
+++ src/tools/ipfsapi.js
@@ -0,0 +1,36 @@
+// 封装ipfs api接口：上传文件、上传内容、根据哈希值获取文件、内容
+const ipfsAPI = require('ipfs-api');
+const addr='119.28.52.50';
+
+const ipfs = ipfsAPI({host: addr, port: '5001', protocol: 'http'});
+
+exports.add = (buffer) =>{
+    return new Promise((resolve,reject)=>{
+        try {
+            ipfs.add(buffer, function (err, files) {
+                if (err || typeof files == "undefined") {
+                    reject(err);
+                } else {
+                    resolve(files[0].hash);
+                }
+            })
+        }catch(ex) {
+            reject(ex);
+        }
+    })
+}
+exports.get = (hash) =>{
+    return new Promise((resolve,reject)=>{
+        try{
+            ipfs.get(hash,function (err,files) {
+                if (err || typeof files == "undefined") {
+                    reject(err);
+                }else{
+                    resolve(files[0].content);
+                }
+            })
+        }catch (ex){
+            reject(ex);
+        }
+    });
+}
